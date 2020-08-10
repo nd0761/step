7b96778930e3b031df0360fe555bdc7e6afe270d
Natalya Denisenko <56645934+nd0761@users.noreply.github.com>
2020-08-10 15:24:54 +0300

    Implement an algorithm for finding free time for a meeting and test it (#12)
    *Create an algorithm to handle optional and mandatory attendees
    
    *Add new tests for optional attendees

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,9 +15,111 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Iterator;
+
+import java.util.Comparator;
 
 public final class FindMeetingQuery {
+  /** Return free slots based on list of time points. */
+  public Collection<TimeRange> getFreeSlots(List<TimePoint> importantEvents, MeetingRequest request) {
+    Collection<TimeRange> possibleSlots = new ArrayList<>();
+    // Sort all time points.
+    Collections.sort(importantEvents, TimePoint.ORDER_END_START);
+
+    // Number of active meeting for the moment under consideration.
+    int numberOfActiveMeetings = 0;
+    int endOfLastMeeting = 0;
+
+    // numberOfActiveMeetings will be 0  - before start and after end of the day,
+    // 1 - if current time slot is free for all mandatory attendees,
+    // >1 - in any other case.
+
+    // numberOfActiveMeetings will increase if new time point under consideration
+    // is a start for a meeting, and decrease in other case.
+    for(TimePoint event : importantEvents) {
+      if (numberOfActiveMeetings == 1) {
+        // Check if there is enough time for requested meeting.
+        if (event.getTime() - endOfLastMeeting >= request.getDuration()) {
+          TimeRange freeSlot = TimeRange.fromStartDuration(
+            endOfLastMeeting, 
+            event.getTime() - endOfLastMeeting
+            );
+          possibleSlots.add(freeSlot);
+        }
+      }
+
+      if (event.getType() == 1) {
+        numberOfActiveMeetings += 1;
+      } else {
+        numberOfActiveMeetings -= 1;
+        endOfLastMeeting = event.getTime();
+      }
+    }
+    return possibleSlots;
+  }
+
+  /** Return list of time slots for current request for mandatory and if possible for optional attendees. */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // Create a list of existing meetings with mandatory attendees.
+    List<TimePoint> importantEvents = new ArrayList<>();
+    // Create a list of existing meetings with optional attendees considered as mandatory.
+    List<TimePoint> optionalEvents = new ArrayList<>();
+  
+    // List of possible time slots for mandatory attendees.
+    Collection<TimeRange> possibleMandatorySlots = new ArrayList<>();
+    // List of possible time slots for optional attendees.
+    Collection<TimeRange> possibleOptionalSlots = new ArrayList<>();
+
+    // Add start and end of the day as time points.
+    TimePoint startOfTheDay = new TimePoint(0, 1, -1);
+    TimePoint endOfTheDay = new TimePoint(24 * 60, 2, -1);
+
+    importantEvents.add(startOfTheDay);
+    importantEvents.add(endOfTheDay);
+
+    optionalEvents.add(startOfTheDay);
+    optionalEvents.add(endOfTheDay);
+
+    for (Event event : events) {
+      // Check if any of mandatory visitors attends this event.
+      boolean mandatoryAttendee = !Collections.disjoint(event.getAttendees(), request.getAttendees());
+      // Check if any of optional visitors attends this event.
+      boolean optionalAttendee = !Collections.disjoint(event.getAttendees(), request.getOptionalAttendees());
+      // Pick events with mandatory attendees (fill list for optional as well).
+
+      TimePoint startOfEvent = new TimePoint(event.getWhen().start(), 1, -1);
+      TimePoint endOfEvent = new TimePoint(event.getWhen().end(), 2, -1);
+      
+      if (mandatoryAttendee) {
+        importantEvents.add(startOfEvent);
+        importantEvents.add(endOfEvent);
+
+        optionalEvents.add(startOfEvent);
+        optionalEvents.add(endOfEvent);
+        continue;
+      }
+      
+      if (optionalAttendee) {
+        optionalEvents.add(startOfEvent);
+        optionalEvents.add(endOfEvent);
+        continue;
+      }
+    }
+
+    // Get possible slots for manadatory and optional attendees.
+    possibleMandatorySlots = getFreeSlots(importantEvents, request);
+    possibleOptionalSlots = getFreeSlots(optionalEvents, request);
+
+    if (possibleOptionalSlots.size() != 0) {
+      return possibleOptionalSlots;
+    }
+    return possibleMandatorySlots;
   }
 }
---
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/TimePoint.java
@@ -0,0 +1,76 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps;
+
+import java.util.Comparator;
+
+/**
+ * Class representing a point of time (either a start of an event or an end of it).
+ */
+public final class TimePoint {
+  /**
+   * A comparator for sorting objects by their time in ascending order.
+   */
+  public static final Comparator<TimePoint> ORDER_END_START = new Comparator<TimePoint>() {
+    @Override
+    public int compare(TimePoint a, TimePoint b) {
+      if (a.getTime() == b.getTime()) {
+        if (a.getType() == 2) {
+          return -1;
+        }
+        if (b.getType() == 2) {
+          return 1;
+        }
+        if (a.getType() == 4) {
+          return -1;
+        }
+        return 1;
+      }
+      return Long.compare(a.getTime(), b.getTime());
+    }
+  };
+
+  private final int time;
+
+  // Type of time point 1 - start of an event for mandatory attendee,
+  // 2 - end of an event for mandatory,
+  // 3 - start for optional, 4 - end for optional.
+  private final int type;
+  // A number of optional attendees for that event,
+  // it will be -1 if there some mandatory attendee for that event.
+  private final int numberOfOptional;
+
+  public TimePoint(int time, int type, int numberOfOptional) {
+    this.time = time;
+    this.type = type;
+    this.numberOfOptional = numberOfOptional;
+  }
+
+  public int getTime() {
+    return time;
+  }
+
+  public int getType() {
+    return type;
+  }
+
+  public int getNumberOfOptional() {
+    return numberOfOptional;
+  }
+
+  public String toString() {
+    return String.format("Time: [%d], Type: [%d]", time, type);
+  }
+}
